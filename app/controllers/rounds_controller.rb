293696a7ba673d9e0f1fe54ef8210190265c2de4
miqs1992 <miqs1992@gmail.com>
2018-02-13 20:06:45 +0100

    Add round views

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -2,11 +2,12 @@ class RoundsController < ApplicationController
     before_action :authorize_admin, only: [:new, :create]
 
     def index
-        @rounds = Round.all
+        @rounds = Round.joins("LEFT JOIN match_days ON rounds.id = match_days.round_id").select("rounds.*, count(match_days.id) as match_day_count").group("rounds.id").order(:id)
     end
 
     def show
-        @round = Round.includes(:match_days).find(params[:id])
+        @round = Round.find(params[:id])
+        @match_days = MatchDay.joins("LEFT JOIN matches ON match_days.id = matches.match_day_id").select("match_days.*, count(matches.id) as match_count").group("match_days.id").order(:day_number)
     end
 
     def new 
@@ -16,15 +17,12 @@ class RoundsController < ApplicationController
     def create
         @round = Round.new(round_params) 
         if @round.save
-            flash[:notice] = "Utworzono nową rundę"
-            redirect_to rounds_path
+            redirect_to rounds_path, notice: "Utworzono nową rundę"
         else
-            flash[:alert] = @round.errors.full_messages.first
-            redirect_to new_round_path
+            redirect_to new_round_path, alert: @round.errors.full_messages.first
         end
     end
     
-
     private
 
     def round_params
